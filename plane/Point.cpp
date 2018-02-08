3ec1ff9c41f75a5dace71c240ed8ee7690b0f48d
Vincent Endrahadi <vincentendrahadi01@gmail.com>
2018-02-08 12:22:34 +0700

    fix errors

--- plane/Point.cpp
+++ plane/Point.cpp
@@ -4,12 +4,12 @@ class Point {
 	private:
 		short x;
 		short y;
-	
+
 	public:
 		// ctor
 		Point(short x = 0, short y = 0) {
-			this.x = x;
-			this.y = y;
+			this->x = x;
+			this->y = y;
 		}
 
 		// getter
@@ -23,11 +23,11 @@ class Point {
 
 		// setter
 		void setX(short x) {
-			this.x = x;
+			this->x = x;
 		}
 
 		void setY(short y) {
-			this.y = y;
+			this->y = y;
 		}
 
 		void scale(Point point, float scaleX, float scaleY) {
@@ -41,8 +41,13 @@ class Point {
 		}
 
 		// angle in degrees
-		void rotate(Point point, float angle) {
-			this.x = sin(angle) * point.x + cos(angle) * point.y;
-			this.y = cos(angle) * point.x - sin(angle) * point.y;
+		void rotate(Point point, double angle) {
+			double angleInRadian = angle * M_PI / 180;
+			double newX = point.x + cos(angleInRadian) *
+				(this->x - point.x) - sin(angleInRadian) * (this->y - point.y);
+			double newY = point.y + sin(angleInRadian) *
+				(this->x - point.x) + cos(angleInRadian) * (this->y - point.y);
+			this->x = (short)round(newX);
+			this->y = (short)round(newY);
 		}
-}
+};
