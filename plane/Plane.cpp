a90aac77fabc458b85c6cfd2927fe08df1bf8d48
andikakusuma <dika.kusuma23@gmail.com>
2018-02-08 13:33:28 +0700

    Add plane class

---
+++ plane/Plane.cpp
@@ -0,0 +1,49 @@
+#include <vector>
+#include "Line.cpp"
+
+using namespace std;
+
+class Plane {
+	protected:
+		vector<Line> lines;
+
+	public:
+		// ctor
+		Plane() {
+			lines = vector<Line>();
+		}
+
+		// cctor
+		Plane(const Plane& plane) {
+			this->lines = vector<Line>();
+			this->lines = plane.lines;
+		}
+
+		// getter
+		vector<Line> getLines() {
+			return lines;
+		}
+
+		void addLine(Line line) {
+			lines.push_back(line);
+		}
+
+		void scale(Point point, float scaleX, float scaleY) {
+			for (auto& iterator : lines) {
+				iterator.scale(point, scaleX, scaleY);
+			}
+		}
+
+		void translate(short deltaX, short deltaY) {
+			for (auto& iterator : lines) {
+				iterator.translate(deltaX, deltaY);
+			}
+		}
+
+		// angle in degrees
+		void rotate(Point point, float angle) {
+			for (auto& iterator : lines) {
+				iterator.rotate(point, angle);
+			}
+		}
+};
